67c4e51fa4328c8be51594e68e411ebbf37c4e11
github-action-benchmark <github@users.noreply.github.com>
2025-05-29 22:17:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c2da831e9b72f868d46a15c0fe63b80c624fbc89

---
+++ docs/benchmarks/pipeline-perf/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1748557079008,
+  "repoUrl": "https://github.com/cijothomas/otel-arrow",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Cijo Thomas",
+            "username": "cijothomas",
+            "email": "cithomas@microsoft.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c2da831e9b72f868d46a15c0fe63b80c624fbc89",
+          "message": "Merge pull request #1 from cijothomas/cijothomas/ci-publish-bench\n\nCijothomas/ci publish bench",
+          "timestamp": "2025-05-29T22:12:40Z",
+          "url": "https://github.com/cijothomas/otel-arrow/commit/c2da831e9b72f868d46a15c0fe63b80c624fbc89"
+        },
+        "date": 1748557078179,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pipeline-perf-collector-config-throughput",
+            "value": 463500,
+            "unit": "logs/sec"
+          },
+          {
+            "name": "pipeline-perf-collector-config-with-batch-processor-throughput",
+            "value": 471000,
+            "unit": "logs/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
